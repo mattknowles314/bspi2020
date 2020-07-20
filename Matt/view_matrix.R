ff1c4892b2cfc191ee930ef3192778e1ee9614e4
Matthew Knowles <mattknowles314@gmail.com>
2020-07-20 14:12:07 +0100

    You can use this file to view the series matrix of the Ross-Innes paper

--- Matt/view_matrix.R
+++ Matt/view_matrix.R
@@ -1,3 +1,5 @@
-#This opens the .txt.gz file from 
-setwd("~/Documents/matthew/BSPIData")
-Data = read.table(gzfile("GSE32222_series_matrix.txt.gz"),sep="\t")
+#This opens the .txt.gz file ncbi.nlm.nih.giv/geo/query/acc.cgi?acc=GSE32222
+dataLocation = "~/Documents/BSPIData/GSE32222_series_matrix.txt.gz" #You will need to change this to the directory your have the file stored in locally
+Data = read.table(gzfile(dataLocation),sep="\t",fill=TRUE)
+
+View(Data)
