cf196a8e2a93e26349e1104c11a961970b3126cd
JessamyA <jsa529@york.ac.uk>
2020-07-21 15:58:11 +0100

    Begun new R script 'DiffBind Demo.R' to run through DiffBind package example data

---
+++ Jessamy/DiffBind Demo.R
@@ -0,0 +1,36 @@
+setwd("~/Work/Internship 2020/gitrepo/Jessamy")
+
+# Package installation
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("DiffBind")
+
+library(DiffBind)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
