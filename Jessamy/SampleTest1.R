e0ee844686877b07cbc087e398697ade42b9101a
JessamyA <jsa529@york.ac.uk>
2020-07-22 15:14:27 +0100

    Partly worked Gviz example, attempts to use Gvis on sample & unzip of .gz file

--- Jessamy/SampleTest1.R
+++ Jessamy/SampleTest1.R
@@ -9,9 +9,12 @@ BiocManager::install("vulcan")
 BiocManager::install("DiffBind")
 BiocManager::install("Gviz")
 
+install.packages("R.utils")
+
 library(vulcan)
 library(DiffBind)
 library(Gviz)
+library(R.utils)
 
 ###################################################################### Load Data
 
@@ -21,13 +24,11 @@ library(Gviz)
 # Details of how to unzip https://stat.ethz.ch/pipermail/r-help/2015-February/425709.html
 
 readLines("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt.gz")
-Sample <- read.table("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt.gz",
+R.utils::gunzip("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt.gz")
+Sample <- read.table("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt",
                      header = TRUE)
 
-###################################################################### Gviz
-
-itrack <- IdeogramTrack(genome = gen, chromosome = chr)
-plotTracks(list(itrack, gtrack, atrack))
+###################################################################### Gviz Exammple
 
 library(GenomicRanges)
 data(cpgIslands)
@@ -36,5 +37,18 @@ chr <- as.character(unique(seqnames(cpgIslands)))
 gen <- genome(cpgIslands)
 atrack <- AnnotationTrack(cpgIslands, name = "CpG")
 
-###################################################################### DiffBind
+itrack <- IdeogramTrack(genome = gen, chromosome = chr)
+plotTracks(list(itrack, gtrack, atrack))
 
+###################################################################### Gviz Sample
+
+data(Sample)
+class(Sample)
+
+gen <- genome(Sample)
+chr <- as.character(unique(Sample$chr))
+itrack <- IdeogramTrack(genome = gen, chromosome = chr)
+atrack2 <- AnnotationTrack(Sample, name = "GSM798383")
+gtrack <- GenomeAxisTrack()
+
+plotTracks(list(gtrack, atrack))
