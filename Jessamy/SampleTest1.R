187856e34eeae101168950dd2e33f1b0d3cda699
JessamyA <jsa529@york.ac.uk>
2020-07-22 13:29:02 +0100

    Example Gviz data

--- Jessamy/SampleTest1.R
+++ Jessamy/SampleTest1.R
@@ -1,2 +1,40 @@
 setwd("~/Work/Internship 2020")
 
+###################################################################### Packages
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("vulcan")
+BiocManager::install("DiffBind")
+BiocManager::install("Gviz")
+
+library(vulcan)
+library(DiffBind)
+library(Gviz)
+
+###################################################################### Load Data
+
+# Sample GSM798383 used as test
+
+# R appears capable of directly reading .gz files without unzipping
+# Details of how to unzip https://stat.ethz.ch/pipermail/r-help/2015-February/425709.html
+
+readLines("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt.gz")
+Sample <- read.table("SampleData/GSM798383_SLX-1201.250.s_4_SLX-1202.250.s_1_peaks.txt.gz",
+                     header = TRUE)
+
+###################################################################### Gviz
+
+itrack <- IdeogramTrack(genome = gen, chromosome = chr)
+plotTracks(list(itrack, gtrack, atrack))
+
+library(GenomicRanges)
+data(cpgIslands)
+class(cpgIslands)
+chr <- as.character(unique(seqnames(cpgIslands)))
+gen <- genome(cpgIslands)
+atrack <- AnnotationTrack(cpgIslands, name = "CpG")
+
+###################################################################### DiffBind
+
