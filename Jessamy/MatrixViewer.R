2a1a86cb255a4bc2307b3d6f64b5c441eebf7df3
JessamyA <jsa529@york.ac.uk>
2020-07-27 22:18:33 +0100

    Edited matrix viewer to have option to export in simple format

--- Jessamy/MatrixViewer.R
+++ Jessamy/MatrixViewer.R
@@ -1,6 +1,8 @@
-
-
 setwd("C:/Users/jessa/Documents/Internship 2020")
+
+################################### VERSION 1 ##########################################
+
+# This version produces a 'Formal class ExpressionSet' SMatrixV2a format
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
@@ -9,5 +11,20 @@ BiocManager::install("GEOquery")
 
 library(GEOquery)
 
-SMatrix = getGEO(filename = "GSE32222_series_matrix.txt")
-view(SMatrix)
+SMatrixV1 = getGEO(filename = "GSE32222_series_matrix.txt")
+
+################################### VERSION 2 ##########################################
+
+# This version produces a classic SMatrixV2aframe style table
+
+SMatrixV2 <- read.table("MatrixEdit.txt", sep = "\t")
+SMatrixV2 <- t(SMatrixV2)
+
+colnames(SMatrixV2) <- as.character(unlist(SMatrixV2[1,]))
+SMatrixV2 = SMatrixV2[-1, ]
+
+View(SMatrixV2)
+
+write.csv(SMatrixV2,
+          "C:/Users/jessa/Documents/Internship 2020\\SMatrix.csv",
+          row.names = FALSE)
--- Tools/MatrixViewer.R
+++ Tools/MatrixViewer.R
@@ -2,6 +2,10 @@
 
 setwd("                 ") # Insert your working directory folder here
 
+################################### VERSION 1 ##########################################
+
+# This version produces a 'Formal class ExpressionSet' SMatrixV2a format
+
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
@@ -10,4 +14,20 @@ BiocManager::install("GEOquery")
 library(GEOquery)
 
 SMatrix = getGEO(filename = "              ") # Insert pathway to your copy of GSE32222_series_matrix.txt here
-view(SMatrix)
+
+################################### VERSION 2 ##########################################
+
+# This version produces a classic SMatrixV2aframe style table
+
+SMatrixV2 <- read.table("MatrixEdit.txt", sep = "\t") # MatrixEdit.txt is a changed copy of the original file available in 'tools'
+SMatrixV2 <- t(SMatrixV2)
+
+colnames(SMatrixV2) <- as.character(unlist(SMatrixV2[1,]))
+SMatrixV2 = SMatrixV2[-1, ]
+
+View(SMatrixV2)
+
+# Optional code to export data to spreadsheet (in format "Path\\filename.csv")
+write.csv(SMatrixV2,
+          "                    \\              ",
+          row.names = FALSE)
