07bea441aff4f307f5e63de176114e6612bb4d2f
Matthew Knowles <mattknowles314@gmail.com>
2020-07-28 16:46:17 +0100

    did some file moving

--- Matt/handler.py
+++ Matt/handler.py
@@ -1,6 +1,8 @@
 import os
 sraLoc = "/home/matthew/Documents/BSPIData/prefetch_deposits/sra"
 fastqLoc = "/home/matthew/Documents/BSPIData/prefetch_deposits/fastq_files"
+wd = "/home/matthew/Documents/BSPI/Matt"
 
-for gsm in os.listdir(sraLoc):
-    os.system("fastq-dump "+sraLoc+"/"+gsm)
+for x in os.listdir(wd):
+    if ".fastq" in x:
+        os.system("mv "+x+" "+fastqLoc)
