3ba21e94ea7395f7e0cf23f4118f842c3068f8da
Matthew Knowles <mattknowles314@gmail.com>
2020-08-05 00:43:29 +0100

    No longer needed

--- Matt/view_matrix.R
+++
@@ -1,5 +0,0 @@
-#This opens the .txt.gz file ncbi.nlm.nih.giv/geo/query/acc.cgi?acc=GSE32222
-dataLocation = "~/Documents/BSPIData/GSE32222_series_matrix.txt.gz" #You will need to change this to the directory your have the file stored in locally
-Data = read.table(gzfile(dataLocation),sep="\t",fill=TRUE)
-
-View(Data)
