10af27b58f450be19ef155ebd18f942f0d9b7bec
Matthew Knowles <mattknowles314@gmail.com>
2020-08-07 21:37:44 +0100

    Reorganising

--- Matt/misc/handler.py
+++
@@ -1,8 +0,0 @@
-import os
-sraLoc = "/home/matthew/Documents/BSPIData/prefetch_deposits/sra"
-fastqLoc = "/home/matthew/Documents/BSPIData/prefetch_deposits/fastq_files"
-wd = "/home/matthew/Documents/BSPI/Matt"
-
-for x in os.listdir(wd):
-    if ".fastq" in x:
-        os.system("mv "+x+" "+fastqLoc)
--- Matt/misc/zipPeaks.py
+++
@@ -1,5 +0,0 @@
-import os
-os.chdir("/home/matthew/Documents/BSPIData/peaks")
-for x in os.listdir():
-    if "summits.bed" not in x:
-        os.system("rm "+x)
