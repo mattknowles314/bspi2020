d356f6de385751243b423b9e44a0a2c4fa4b6447
Matthew Knowles <mattknowles314@gmail.com>
2020-08-07 22:44:10 +0100

    removed plotting from scripts, this can be run from terminal once the script is ran

--- Matt/rscripts/run_diffbind.R
+++ Matt/rscripts/run_diffbind.R
@@ -4,15 +4,10 @@ library(DiffBind)
 samples <- read.csv("labelled_2.csv")
 samples_dba <- dba(sampleSheet = "labelled_2.csv",minOverlap = 0,config = data.frame(reportInit="DBA", DataType=DBA_DATA_GRANGES, AnalysisMethod=DBA_DESEQ2),peakCaller = "bed",bRemoveM = TRUE, attributes = c(DBA_CONDITION, DBA_TISSUE))
 
-samples_count<-dba.count(samples_dba, summits=350, bParallel = FALSE)
-plot(samples_count)
+samples_count<-dba.count(samples_dba, summits=250, bParallel = FALSE)
 
 samples_contrast <- dba.contrast(samples_count, categories = c(DBA_CONDITION,DBA_TISSUE))
-plot(samples_contrast)
 
 sample_analyze <- dba.analyze(samples_contrast, method=DBA_DESEQ2, bFullLibrarySize=TRUE)
-plot(sample_analyze)
-
-dba.plotHeatmap(sample_analyze,contrast = 1)
 
 samples.DB <- dba.report(sample_analyze)
