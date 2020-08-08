62425973624e539a980c8dab972b26fa27e4596c
Matthew Knowles <mattknowles314@gmail.com>
2020-08-08 12:05:56 +0100

    Can be run now to save plots automatically!

--- Matt/rscripts/run_diffbind.R
+++ Matt/rscripts/run_diffbind.R
@@ -1,13 +1,24 @@
 setwd("/home/matthew/Documents/BSPIData/")
+
 library(DiffBind)
 
+#Run DiffBind Analysis
 samples <- read.csv("labelled_2.csv")
-samples_dba <- dba(sampleSheet = "labelled_2.csv",minOverlap = 0,config = data.frame(reportInit="DBA", DataType=DBA_DATA_GRANGES, AnalysisMethod=DBA_DESEQ2),peakCaller = "bed",bRemoveM = TRUE, attributes = c(DBA_CONDITION, DBA_TISSUE))
-
-samples_count<-dba.count(samples_dba, summits=350, bParallel = FALSE)
-
+samples_dba <- dba(sampleSheet = samples, config = data.frame(reportInit="DBA", DataType=DBA_TISSUE, AnalysisMethod=DBA_DESEQ2),peakCaller = "bed",bRemoveM = TRUE)
+samples_count<-dba.count(samples_dba, bParallel = FALSE)
 samples_contrast <- dba.contrast(samples_count, categories = c(DBA_CONDITION,DBA_TISSUE))
+samples_analyze <- dba.analyze(samples_contrast, method=DBA_DESEQ2, bFullLibrarySize=TRUE)
+samples.DB <- dba.report(samples_analyze)
 
-sample_analyze <- dba.analyze(samples_contrast, method=DBA_DESEQ2, bFullLibrarySize=TRUE)
+#Produce Plots
+png(file="~/Documents/BSPIData/plots/pca.png")
+dba.plotPCA(samples_analyze, DBA_TISSUE, label=DBA_CONDITION)
+dev.off()
 
-samples.DB <- dba.report(sample_analyze)
+png(file="~/Documents/BSPIData/plots/ma.png")
+dba.plotMA(samples_analyze)
+dev.off()
+
+png(file="~/Documents/BSPIData.plots.hmp.png")
+dba.plotHeatmap(samples_analyze, contrast=1, correlations=FALSE )
+dev.off()
