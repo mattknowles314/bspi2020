c4212429df676d2d87bab04585addf1339251ea8
Matthew Knowles <mattknowles314@gmail.com>
2020-08-11 14:41:09 +0100

    Put everything into functions to make life easier

--- Matt/rscripts/run_diffbind.R
+++ Matt/rscripts/run_diffbind.R
@@ -1,32 +1,48 @@
 setwd("/home/matthew/Documents/BSPIData/")
 
-library(DiffBind)
+library(DiffBind, quietly=TRUE)
 
-#Run DiffBind Analysis
-samples <- read.csv("labelled_data.csv")
-samples_dba <- dba(sampleSheet = samples, config = data.frame(reportInit="DBA", DataType=DBA_TISSUE, AnalysisMethod=DBA_DESEQ2), bRemoveM = TRUE)
-samples_count<-dba.count(samples_dba,  bParallel = FALSE)
-samples_contrast <- dba.contrast(samples_count, categories = c(DBA_CONDITION,DBA_TISSUE))
-samples_analyze <- dba.analyze(samples_count, method=DBA_DESEQ2, bFullLibrarySize=TRUE)
-samples.DB <- dba.report(samples_analyze, method = DBA_DESEQ2, bAll = TRUE, bUsePval=TRUE)
+analysis <- function(){
+    samples <- read.csv("labelled_data.csv")
+    samples_dba <- dba(sampleSheet = samples, config = data.frame(reportInit="DBA", DataType=DBA_TISSUE, AnalysisMethod=DBA_DESEQ2), bRemoveM = TRUE)
+    save(samples_dba, file = "samplesdba.Rdata")
+    samples_count<-dba.count(samples_dba,  bParallel = FALSE)
+    save(samples_count, file = "samplescount.Rdata")
+    samples_contrast <- dba.contrast(samples_count, categories = c(DBA_CONDITION,DBA_TISSUE))
+    save(samples_contrast, file = "samplescont.Rdata")
+    samples_analyze <- dba.analyze(samples_count, method=DBA_DESEQ2, bParallel= FALSE)
+    save(samples_analyze, file = "samplesanalyze.Rdata")
+    samples.DB <- dba.report(samples_analyze, method = DBA_DESEQ2, bUsePval=TRUE)
+    save(samples.DB, file = "samplesDB.Rdata")
+}
 
-#Produce Plots
-png(file="~/Documents/BSPIData/plots/pcadata.png")
-dba.plotPCA(samples_analyze, DBA_TISSUE, label=DBA_CONDITION)
-dev.off()
+plots <- function(samples_analyze)
+{    
+    png(file="~/Documents/BSPIData/plots/pcadata.png")
+    dba.plotPCA(samples_analyze, DBA_TISSUE, label=DBA_CONDITION, b3D = FALSE)
+    dev.off()
 
-png(file="~/Documents/BSPIData/plots/madata.png")
-dba.plotMA(samples_analyze, bUsePval = TRUE)
-dev.off()
+    png(file="~/Documents/BSPIData/plots/madata.png")
+    dba.plotMA(samples_analyze, bUsePval = FALSE)
+    dev.off()
 
-png(file="~/Documents/BSPIData/plots/hmpdata.png")
-dba.plotHeatmap(samples_analyze, contrast=1, bUsePval = TRUE)
-dev.off()
+    png(file="~/Documents/BSPIData/plots/hmpdata.png")
+    dba.plotHeatmap(samples_analyze, contrast=1, bUsePval = TRUE)
+    dev.off()
 
-png(file="~/Documents/BSPIData/plots/volcdata.png")
-dba.plotVolcano(samples_analyze, bUsePval = TRUE)
-dev.off()
+    png(file="~/Documents/BSPIData/plots/volcdata.png")
+    dba.plotVolcano(samples_analyze, bUsePval = TRUE)
+    dev.off()
 
-png(file="~/Documents/BSPIData/plots/boxdata.png")
-dba.plotBox(samples_analyze, bUsePval = 1, contrast = 1)
-dev.off()
+    png(file="~/Documents/BSPIData/plots/boxdata.png")
+    dba.plotBox(samples_analyze, contrast = 2)
+    dev.off()
+}
+
+load_files <- function(){
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplesanalyze.Rdata")
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplescont.Rdata")
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplescount.Rdata")
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplesDB.Rdata")
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplesdba.Rdata")
+}
