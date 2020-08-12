84e1f2c5f980d8031e1bc51b8d65d783f324efa4
Matthew Knowles <mattknowles314@gmail.com>
2020-08-12 14:56:21 +0100

    working on running viking

--- Matt/rscripts/run_vulcan.R
+++ Matt/rscripts/run_vulcan.R
@@ -8,5 +8,51 @@ load_files <- function(){
   load(file = "/home/matthew/Documents/BSPIData/rdata/samplescount.Rdata")
   load(file = "/home/matthew/Documents/BSPIData/rdata/samplesDB.Rdata")
   load(file = "/home/matthew/Documents/BSPIData/rdata/samplesdba.Rdata")
+  load(file = "/home/matthew/Documents/BSPIData/rdata/samplespeaks.Rdata")
 }
 
+import <- function (sheetfile, intervals = 210, samples_dba) 
+{
+  sheet <- read.csv(sheetfile, as.is = TRUE)
+  dbobj <- samples_dba
+  message("Sheet loaded. You have ", nrow(sheet), " samples and ", 
+          length(unique(sheet$Condition)), " conditions")
+  dbcounts <- DiffBind::dba.count(dbobj, summits = intervals,  bParallel = FALSE)
+  listcounts <- dbcounts$peaks
+  names(listcounts) <- dbcounts$samples[2, 1]
+  first <- listcounts[[1]]
+  rawmat <- matrix(NA, nrow = nrow(first), ncol = length(listcounts) + 
+                     3)
+  colnames(rawmat) <- c("Chr", "Start", "End", names(listcounts))
+  rownames(rawmat) <- 1:nrow(rawmat)
+  rawmat <- as.data.frame(rawmat)
+  rawmat[, 1] <- as.character(first[, 1])
+  rawmat[, 2] <- as.integer(first[, 2])
+  rawmat[, 3] <- as.integer(first[, 3])
+  for (i in seq_len(length(listcounts))) {
+    rawmat[, names(listcounts)[i]] <- as.numeric(listcounts[[i]]$RPKM)
+  }
+  peakrpkms <- rawmat
+  first <- listcounts[[1]]
+  rawmat <- matrix(NA, nrow = nrow(first), ncol = length(listcounts) + 
+                     3)
+  colnames(rawmat) <- c("Chr", "Start", "End", names(listcounts))
+  rownames(rawmat) <- 1:nrow(rawmat)
+  rawmat <- as.data.frame(rawmat)
+  rawmat[, 1] <- as.character(first[, 1])
+  rawmat[, 2] <- as.integer(first[, 2])
+  rawmat[, 3] <- as.integer(first[, 3])
+  for (i in seq_len(length(listcounts))) {
+    rawmat[, names(listcounts)[i]] <- as.integer(listcounts[[i]]$Reads)
+  }
+  peakcounts <- rawmat
+  samples <- list()
+  conditions <- unique(sheet$Condition)
+  for (cond in conditions) {
+    heresamples <- sheet$SampleID[sheet$Condition == cond]
+    samples[[cond]] <- heresamples
+  }
+  vobj <- list(peakcounts = peakcounts, samples = samples, 
+               peakrpkms = peakrpkms)
+  return(vobj)
+}
