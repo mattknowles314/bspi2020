61c75f6348db1b311a25ea996844fd43f74d690c
Matthew Knowles <mattknowles314@gmail.com>
2020-08-20 15:10:53 +0100

    Now auto-compiles a LaTeX file which puts all the results of the pipeline into one nice document

--- Matt/analysis_pipeline.py
+++ Matt/analysis_pipeline.py
@@ -6,6 +6,8 @@ run_vulcan_loc = "/home/matthew/Documents/BSPI/Matt/rscripts/run_vulcan.R"
 db_test_loc = "/home/matthew/Documents/BSPI/Matt/rscripts/testdb.R"
 rdata_loc = "/home/matthew/Documents/BSPIData/rdata"
 rplot_loc = "/home/matthew/Documents/BSPIData/plots"
+report_loc = "/home/matthew/Documents/BSPI/Matt/reports/results.tex"
+
 
 print("---RUNNING DIFFBIND---")
 os.system("Rscript "+run_diffbind_loc)
@@ -25,6 +27,8 @@ print("---RUNNING DIFFBIND TEST---")
 if(os.system("Rscript "+db_test_loc)==0):
     print("---RUNNING VULCAN---")
     os.system("Rscripti "+run_vulcan_loc)
+    os.system("pdflatex "+report_loc)
+    os.system("zathura "+report_loc.strip("tex")+"pdf")
     print("---FINISHED---")
 else:
     print("---FAILED---")
